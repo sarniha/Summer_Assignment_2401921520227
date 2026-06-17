59a4f927c9073a6637ee4aa5814c8c5207bc497a
unknown <sarniha12@gmail.com>
2026-06-17 21:22:22 +0530

    day_4 problems done

---
+++ Week_2/Day_4/revstring.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public String reverseWords(String s) {
+        char[] arr = s.toCharArray();
+
+        int start = 0;
+
+        for (int end = 0; end <= arr.length; end++) {
+
+            // End of a word or end of string
+            if (end == arr.length || arr[end] == ' ') {
+                reverse(arr, start, end - 1);
+                start = end + 1;
+            }
+        }
+
+        return new String(arr);
+    }
+}
